dde29e6f880a35aa303e7a4dc1e8f1b57f7b5684
Parth Intwala <parthintwala@Parths-MacBook-Air.local>
2021-01-28 22:23:18 +0530

    Add: DLL, DAL, Utilities Project created

---
+++ UnicoVehicle/UnicoVehicle.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace UnicoVehicle.DAL
+{
+    public class Class1
+    {
+    }
+}
---
+++ UnicoVehicle/UnicoVehicle.Utilities/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace UnicoVehicle.Utilities
+{
+    public class Class1
+    {
+    }
+}
